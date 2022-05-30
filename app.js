a2a1ee1a9b38e3c60165062daa136208fee56a41
VINOD KUMAR <98078889+vinod4848@users.noreply.github.com>
2022-05-30 19:57:32 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+
+
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const { ApolloServer } = require("apollo-server-express");
+
+const schema = require("./schema");
+
+const URL = "mongodb://localhost:27017/moviesdb";
+
+const connect = mongoose.connect(URL, { useNewUrlParser: true });
+connect.then(
+	db => {
+		console.log("Connected correctly to server!");
+	},
+	err => {
+		console.log(err);
+	},
+);
+
+const server = new ApolloServer({
+	schema: schema,
+});
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use("*", cors());
+
+server.applyMiddleware({ app });
+
+app.listen({ port: 4000 }, () =>
+	console.log(` Server ready at http://localhost:4000${server.graphqlPath}`),
+);
